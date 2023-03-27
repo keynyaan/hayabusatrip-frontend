72918b98453fcff389de10740f246fef85f6e2d5
keena <keynyaan@gmail.com>
2023-03-27 14:59:30 +0900

    feat: create nextjs #1

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css';
 import 'tailwindcss/tailwind.css';
 import type { AppProps } from 'next/app';
+import React from 'react';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
