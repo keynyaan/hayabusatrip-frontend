f0b3d0f7f3d7727746800369cc22ea5fc056b6ac
keena <keynyaan@gmail.com>
2023-03-30 13:57:24 +0900

    style: ESLint、Prettierを実行 #9

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,15 +1,13 @@
-import type { NextPage } from 'next';
-import React from 'react';
+import type { NextPage } from 'next'
+import React from 'react'
 
 const About: NextPage = () => {
   return (
     <div>
-      <h1 className="text-blue-700 underline decoration-gray-500">
-        Hello World!
-      </h1>
+      <h1 className="text-blue-700 underline decoration-gray-500">Hello World!</h1>
       <p>Hello GitHub Actions!</p>
     </div>
-  );
-};
+  )
+}
 
-export default About;
+export default About
