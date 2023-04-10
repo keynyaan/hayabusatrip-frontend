27021acaccf9c1ce1b8b25a9650ba48275a3f057
keena <keynyaan@gmail.com>
2023-04-10 15:33:27 +0900

    feat: Firebase導入に伴うJestの設定変更 #47

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 // jest.config.js
-const nextJest = require('next/jest');
+const nextJest = require('next/jest')
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
-});
+})
 
 // Add any custom config to be passed to Jest
 /** @type {import('jest').Config} */
@@ -13,7 +13,10 @@ const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 
   testEnvironment: 'jest-environment-jsdom',
-};
+}
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(customJestConfig);
+module.exports = async () => ({
+  ...(await createJestConfig(customJestConfig)()),
+  transformIgnorePatterns: ['node_modules/(?!(firebase|@firebase))'],
+})
