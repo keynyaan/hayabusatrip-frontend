11255564162c625f25aa90bea8016253214b87e1
keena <keynyaan@gmail.com>
2023-04-29 23:14:06 +0900

    feat: ユーザー情報のAPI作成(取得、作成、更新) #69

---
+++ src/api/userApi.ts
@@ -0,0 +1,92 @@
+import axios from 'axios'
+import { usersUrl } from '@/utils/url'
+import {
+  CREATE_USER_ERROR_MSG,
+  GET_USER_ERROR_MSG,
+  UPDATE_USER_ERROR_MSG,
+} from '@/utils/constants'
+
+type CreateUserOptions = {
+  uid: string
+  name: string
+  icon_path?: string
+}
+
+type UpdateUserOptions = {
+  uid: string
+  name?: string
+  icon_path?: string
+  request_count?: number
+  last_reset_date?: string
+  last_login_time?: string
+}
+
+// 全てのユーザー情報の取得
+export const getUsers = async (idToken: string) => {
+  try {
+    const res = await axios.get(usersUrl, {
+      headers: {
+        Authorization: `Bearer ${idToken}`,
+      },
+    })
+    return res.data
+  } catch (e) {
+    throw new Error(GET_USER_ERROR_MSG)
+  }
+}
+
+// 特定のユーザー情報の取得
+export const getUser = async (idToken: string, uid: string) => {
+  try {
+    const res = await axios.get(`${usersUrl}/${uid}`, {
+      headers: {
+        Authorization: `Bearer ${idToken}`,
+      },
+    })
+    return res.data
+  } catch (e) {
+    throw new Error(GET_USER_ERROR_MSG)
+  }
+}
+
+// 特定のユーザー情報の取得
+export const createUser = async (
+  idToken: string,
+  options: CreateUserOptions
+) => {
+  try {
+    const params: { user: CreateUserOptions } = {
+      user: options,
+    }
+
+    const res = await axios.post(usersUrl, params, {
+      headers: {
+        Authorization: `Bearer ${idToken}`,
+      },
+    })
+    return res.data
+  } catch (e) {
+    throw new Error(CREATE_USER_ERROR_MSG)
+  }
+}
+
+// ユーザー情報の更新
+export const updateUser = async (
+  idToken: string,
+  options: UpdateUserOptions
+) => {
+  try {
+    const params: { user: UpdateUserOptions } = {
+      user: options,
+    }
+
+    const res = await axios.patch(`${usersUrl}/${options.uid}`, params, {
+      headers: {
+        Authorization: `Bearer ${idToken}`,
+      },
+    })
+    return res.data
+  } catch (e) {
+    throw new Error(UPDATE_USER_ERROR_MSG)
+  }
+}
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,17 +2,22 @@ import React from 'react'
 import { Confetti } from '@/components/Confetti'
 import { Header } from '@/components/Header'
 import { Footer } from '@/components/Footer'
+import { useAuthContext } from '@/context/AuthContext'
 
 type LayoutProps = {
   children: React.ReactNode
 }
 
 export const Layout: React.FC<LayoutProps> = ({ children }) => {
+  const { firstLogin } = useAuthContext()
+
   return (
     <div className="flex flex-col min-h-screen relative">
-      <div className="z-20">
-        <Confetti />
-      </div>
+      {firstLogin && (
+        <div className="z-20">
+          <Confetti />
+        </div>
+      )}
       <Header />
       <main className="pt-24 px-4 flex-grow">{children}</main>
       <Footer />
