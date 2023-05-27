75f915090e17b60fce6d5557e3a470aca8e37e93
keena <keynyaan@gmail.com>
2023-05-27 17:22:16 +0900

    feat: 旅行プランの作成機能の追加 #96

---
+++ src/api/tripApi.ts
@@ -0,0 +1,144 @@
+import axios from 'axios'
+import { USERS_URL, TRIPS_URL } from '@/utils/constants'
+
+export type DbTripData = {
+  id: number
+  user_id: number
+  prefecture_id: number
+  title: string
+  start_date: string
+  end_date: string
+  memo: string
+  image_path: string
+  is_public: boolean
+  trip_token: string
+  created_at: string
+  updated_at: string
+}
+
+export type CreateTripOptions = {
+  user_id: number
+  prefecture_id: number
+  title: string
+  start_date: string
+  end_date: string
+}
+
+export type UpdateTripOptions = {
+  prefecture_id?: number
+  title?: string
+  start_date?: string
+  end_date?: string
+  memo?: string
+  image_path?: string
+  is_public?: boolean
+}
+
+// ユーザーの全ての旅行プランの取得
+export const getTripsAPI = async (idToken: string, user_uid: string) => {
+  try {
+    const res = await axios.get(`${USERS_URL}/${user_uid}/${TRIPS_URL}`, {
+      headers: {
+        Authorization: `Bearer ${idToken}`,
+      },
+    })
+    return res.data
+  } catch (e) {
+    throw e
+  }
+}
+
+// 特定の旅行プランの取得
+export const getTripAPI = async (
+  idToken: string,
+  user_uid: string,
+  trip_token: string
+) => {
+  try {
+    const res = await axios.get(
+      `${USERS_URL}/${user_uid}/${TRIPS_URL}/${trip_token}`,
+      {
+        headers: {
+          Authorization: `Bearer ${idToken}`,
+        },
+      }
+    )
+    return res.data
+  } catch (e) {
+    throw e
+  }
+}
+
+// 旅行プランの作成
+export const createTripAPI = async (
+  idToken: string,
+  user_uid: string,
+  options: CreateTripOptions
+) => {
+  try {
+    const params: { trip: CreateTripOptions } = {
+      trip: options,
+    }
+
+    const res = await axios.post(
+      `${USERS_URL}/${user_uid}/${TRIPS_URL}`,
+      params,
+      {
+        headers: {
+          Authorization: `Bearer ${idToken}`,
+        },
+      }
+    )
+    return res.data
+  } catch (e) {
+    throw e
+  }
+}
+
+// 旅行プランの更新
+export const updateTripAPI = async (
+  idToken: string,
+  user_uid: string,
+  trip_token: string,
+  options: UpdateTripOptions
+) => {
+  try {
+    const params: { trip: UpdateTripOptions } = {
+      trip: options,
+    }
+
+    const res = await axios.patch(
+      `${USERS_URL}/${user_uid}/${TRIPS_URL}/${trip_token}`,
+      params,
+      {
+        headers: {
+          Authorization: `Bearer ${idToken}`,
+        },
+      }
+    )
+    return res.data
+  } catch (e) {
+    throw e
+  }
+}
+
+// 旅行プランの削除
+export const deleteTripAPI = async (
+  idToken: string,
+  user_uid: string,
+  trip_token: string
+) => {
+  try {
+    const res = await axios.delete(
+      `${USERS_URL}/${user_uid}/${TRIPS_URL}/${trip_token}`,
+      {
+        headers: {
+          Authorization: `Bearer ${idToken}`,
+        },
+      }
+    )
+    return res.data
+  } catch (e) {
+    throw e
+  }
+}
---
+++ src/hooks/useTripApi.ts
@@ -0,0 +1,122 @@
+import {
+  getTripsAPI,
+  getTripAPI,
+  createTripAPI,
+  updateTripAPI,
+  deleteTripAPI,
+  CreateTripOptions,
+  UpdateTripOptions,
+  DbTripData,
+} from '@/api/tripApi'
+import { useAuthContext } from '@/context/AuthContext'
+import { useToast } from '@/context/ToastContext'
+import {
+  CREATE_TRIP_SUCCESS_MSG,
+  UPDATE_TRIP_SUCCESS_MSG,
+  DELETE_TRIP_SUCCESS_MSG,
+  GET_TRIP_ERROR_MSG,
+  CREATE_TRIP_ERROR_MSG,
+  UPDATE_TRIP_ERROR_MSG,
+  DELETE_TRIP_ERROR_MSG,
+} from '@/utils/constants'
+
+export const useTripApi = () => {
+  const { showToast } = useToast()
+  const { setTripApiLoading } = useAuthContext()
+  const getTrips = async (idToken: string, user_uid: string) => {
+    setTripApiLoading(true)
+    try {
+      const data: DbTripData[] = await getTripsAPI(idToken, user_uid)
+      return data
+    } catch (e) {
+      showToast('error', GET_TRIP_ERROR_MSG)
+    } finally {
+      setTripApiLoading(false)
+    }
+  }
+
+  const getTrip = async (
+    idToken: string,
+    user_uid: string,
+    trip_token: string
+  ) => {
+    setTripApiLoading(true)
+    try {
+      const data: DbTripData = await getTripAPI(idToken, user_uid, trip_token)
+      return data
+    } catch (e) {
+      showToast('error', GET_TRIP_ERROR_MSG)
+    } finally {
+      setTripApiLoading(false)
+    }
+  }
+
+  const createTrip = async (
+    idToken: string,
+    user_uid: string,
+    options: CreateTripOptions
+  ) => {
+    setTripApiLoading(true)
+    try {
+      const data: DbTripData = await createTripAPI(idToken, user_uid, options)
+      showToast('success', CREATE_TRIP_SUCCESS_MSG)
+      return data
+    } catch (e) {
+      showToast('error', CREATE_TRIP_ERROR_MSG)
+    } finally {
+      setTripApiLoading(false)
+    }
+  }
+
+  const updateTrip = async (
+    idToken: string,
+    user_uid: string,
+    trip_token: string,
+    options: UpdateTripOptions
+  ) => {
+    setTripApiLoading(true)
+    try {
+      const data: DbTripData = await updateTripAPI(
+        idToken,
+        user_uid,
+        trip_token,
+        options
+      )
+      showToast('success', UPDATE_TRIP_SUCCESS_MSG)
+      return data
+    } catch (e) {
+      showToast('error', UPDATE_TRIP_ERROR_MSG)
+    } finally {
+      setTripApiLoading(false)
+    }
+  }
+
+  const deleteTrip = async (
+    idToken: string,
+    user_uid: string,
+    trip_token: string
+  ) => {
+    setTripApiLoading(true)
+    try {
+      const data: DbTripData = await deleteTripAPI(
+        idToken,
+        user_uid,
+        trip_token
+      )
+      showToast('success', DELETE_TRIP_SUCCESS_MSG)
+      return data
+    } catch (e) {
+      showToast('error', DELETE_TRIP_ERROR_MSG)
+    } finally {
+      setTripApiLoading(false)
+    }
+  }
+
+  return {
+    getTrips,
+    getTrip,
+    createTrip,
+    updateTrip,
+    deleteTrip,
+  }
+}
