23d6ddf2e2e2c4a9d9f656a69f4b44f946d7d044
keena <keynyaan@gmail.com>
2023-06-17 14:01:59 +0900

    refactor: DividerWithTextのスタイル修正 #120

--- src/components/DividerWithText.tsx
+++ src/components/DividerWithText.tsx
@@ -8,9 +8,9 @@ export const DividerWithText: React.FC<DividerWithTextProps> = ({ text }) => {
   return (
     <>
       <div className="flex items-center justify-center m-4">
-        <hr className="w-20 border-t-2 border-gray-200" />
-        <p className="text-gray-700 mx-6">{text}</p>
-        <hr className="w-20 border-t-2 border-gray-200" />
+        <hr className="w-[25%] border-t-2 border-gray-200" />
+        <p className="text-gray-700 mx-6 text-sm">{text}</p>
+        <hr className="w-[25%] border-t-2 border-gray-200" />
       </div>
     </>
   )
