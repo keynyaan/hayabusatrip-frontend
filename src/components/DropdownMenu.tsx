b05d08e9c9fc6f3279b732af168b529304f7c95f
keena <keynyaan@gmail.com>
2023-07-11 13:06:27 +0900

    refactor: TripCardのリファクタリング #178

--- src/components/DropdownMenu.tsx
+++ src/components/DropdownMenu.tsx
@@ -3,14 +3,20 @@ import React, { ReactNode } from 'react'
 type DropdownMenuProps = {
   isVisible: boolean
   children: ReactNode
+  isTop0?: boolean
 }
 
 export const DropdownMenu: React.FC<DropdownMenuProps> = ({
   isVisible,
   children,
+  isTop0,
 }) => {
   return isVisible ? (
-    <div className="absolute z-15 bg-white text-gray-700 rounded shadow right-0 mt-2 flex flex-col">
+    <div
+      className={`absolute z-15 bg-white text-gray-700 rounded shadow right-0 mt-2 flex flex-col ${
+        isTop0 ? 'top-0' : ''
+      }`}
+    >
       {React.Children.map(children, (child) => child)}
     </div>
   ) : null
--- src/components/TripCard.tsx
+++ src/components/TripCard.tsx
@@ -64,172 +64,172 @@ export const TripCard: React.FC<TripCardProps> = ({
 
   if (!trip) return null
 
+  const tripImage = (
+    <Image
+      src={trip.image_path}
+      alt={`${trip.title}の旅行画像`}
+      fill
+      priority={true}
+      className={`object-cover ${isDetailPage ? '' : 'rounded-t-md'}`}
+    />
+  )
+
+  const tripDescription = (
+    <>
+      <div
+        className={`flex justify-center items-center mb-2 ${
+          isDetailPage ? 'text-lg' : 'text-sm'
+        }`}
+      >
+        {!viewMode && (
+          <FontAwesomeIcon
+            icon={trip.is_public ? faUnlock : faLock}
+            className="mr-2"
+          />
+        )}
+        <h3
+          className={`font-bold ${
+            isDetailPage
+              ? ''
+              : 'overflow-hidden whitespace-nowrap text-ellipsis'
+          }`}
+        >
+          {trip.title}
+        </h3>
+      </div>
+      <div
+        className={`flex justify-between ${
+          isDetailPage ? 'text-sm' : 'text-xs'
+        }`}
+      >
+        <p className="text-gray-500">
+          {formatDate(trip.start_date)}-{formatDate(trip.end_date)}
+        </p>
+        <span>
+          <FontAwesomeIcon icon={faLocationDot} className="mr-1" />
+          {TRIP_DESTINATION_ITEMS[trip.prefecture_id - 1]?.name}
+        </span>
+      </div>
+    </>
+  )
+
+  const dropDownMenu = (
+    <div className="absolute top-0 right-0">
+      <div
+        className="m-2 w-10 h-10 rounded-full bg-gray-500 bg-opacity-80 hover:bg-opacity-60 transition p-1 flex items-center justify-center cursor-pointer"
+        onClick={toggleDropdown}
+      >
+        <FontAwesomeIcon icon={faEllipsis} className="text-white text-2xl" />
+      </div>
+      <DropdownMenu isVisible={isDropdownVisible} isTop0={true}>
+        {isDetailPage && (
+          <DropdownMenuButton
+            onClick={() => onOpenModal(setTripPhotoOpen)}
+            label="写真の変更"
+          />
+        )}
+        <DropdownMenuButton
+          onClick={() => onOpenModal(setTripPublishSettingsOpen)}
+          label="公開状態の変更"
+        />
+        <DropdownMenuButton
+          onClick={() => onOpenModal(setTripTitleOpen)}
+          label="タイトルの変更"
+        />
+        <DropdownMenuButton
+          onClick={() => onOpenModal(setTripDestinationOpen)}
+          label="旅行先の変更"
+        />
+        {isDetailPage ?? (
+          <DropdownMenuButton
+            onClick={() => onOpenModal(setCopyTripOpen)}
+            label="コピー"
+          />
+        )}
+        <DropdownMenuButton
+          onClick={() => onOpenModal(setDeleteTripOpen)}
+          label="削除"
+          className="text-red-500"
+        />
+      </DropdownMenu>
+      {tripPhotoOpen && (
+        <TripCardModal
+          open={tripPhotoOpen}
+          onClose={() => onCloseModal(setTripPhotoOpen)}
+          form={FORM_TRIP_PHOTO}
+        />
+      )}
+      {tripPublishSettingsOpen && (
+        <TripCardModal
+          open={tripPublishSettingsOpen}
+          onClose={() => onCloseModal(setTripPublishSettingsOpen)}
+          form={FORM_TRIP_PUBLISH_SETTINGS}
+        />
+      )}
+      {tripTitleOpen && (
+        <TripCardModal
+          open={tripTitleOpen}
+          onClose={() => onCloseModal(setTripTitleOpen)}
+          form={FORM_TRIP_TITLE}
+        />
+      )}
+      {tripDestinationOpen && (
+        <TripCardModal
+          open={tripDestinationOpen}
+          onClose={() => onCloseModal(setTripDestinationOpen)}
+          form={FORM_TRIP_DESTINATION}
+        />
+      )}
+      {copyTripOpen && (
+        <TripCardModal
+          open={copyTripOpen}
+          onClose={() => onCloseModal(setCopyTripOpen)}
+          form={FORM_COPY_TRIP}
+        />
+      )}
+      {deleteTripOpen && (
+        <TripCardModal
+          open={deleteTripOpen}
+          onClose={() => onCloseModal(setDeleteTripOpen)}
+          form={FORM_DELETE_TRIP}
+        />
+      )}
+    </div>
+  )
+
   return (
     <div
-      className={
+      className={`relative ${
         isDetailPage
           ? ''
-          : 'rounded-xl overflow-hidden shadow-md hover:shadow-xl transition'
-      }
+          : 'rounded-lg overflow-hidden shadow-md hover:shadow-xl transition'
+      }`}
       key={trip.id}
       ref={dropdownRef}
     >
-      <div className="relative pb-[66.666667%]">
-        {isDetailPage ? (
-          <div className="absolute inset-0">
-            <Image
-              src={trip.image_path}
-              alt={`${trip.title}の旅行画像`}
-              fill
-              priority={true}
-              className="object-cover"
-            />
-          </div>
-        ) : (
-          <Link href={`${TRIPS_URL}/${trip.trip_token}`}>
-            <div className="absolute inset-0">
-              <Image
-                src={trip.image_path}
-                alt={`${trip.title}の旅行画像`}
-                fill
-                priority={true}
-                className="object-cover"
-              />
-            </div>
-          </Link>
-        )}
-        {!viewMode && (
-          <>
-            <div
-              className="absolute top-0 right-0 m-2 w-10 h-10 rounded-full bg-gray-500 bg-opacity-80 hover:bg-opacity-60 transition p-1 flex items-center justify-center cursor-pointer"
-              onClick={toggleDropdown}
-            >
-              <FontAwesomeIcon
-                icon={faEllipsis}
-                className="text-white text-2xl"
-              />
-            </div>
-            <DropdownMenu isVisible={isDropdownVisible}>
-              {isDetailPage && (
-                <DropdownMenuButton
-                  onClick={() => onOpenModal(setTripPhotoOpen)}
-                  label="写真の変更"
-                />
-              )}
-              <DropdownMenuButton
-                onClick={() => onOpenModal(setTripPublishSettingsOpen)}
-                label="公開状態の変更"
-              />
-              <DropdownMenuButton
-                onClick={() => onOpenModal(setTripTitleOpen)}
-                label="タイトルの変更"
-              />
-              <DropdownMenuButton
-                onClick={() => onOpenModal(setTripDestinationOpen)}
-                label="旅行先の変更"
-              />
-              {isDetailPage ?? (
-                <DropdownMenuButton
-                  onClick={() => onOpenModal(setCopyTripOpen)}
-                  label="コピー"
-                />
-              )}
-              <DropdownMenuButton
-                onClick={() => onOpenModal(setDeleteTripOpen)}
-                label="削除"
-                className="text-red-500"
-              />
-            </DropdownMenu>
-            {tripPhotoOpen && (
-              <TripCardModal
-                open={tripPhotoOpen}
-                onClose={() => onCloseModal(setTripPhotoOpen)}
-                form={FORM_TRIP_PHOTO}
-              />
-            )}
-            {tripPublishSettingsOpen && (
-              <TripCardModal
-                open={tripPublishSettingsOpen}
-                onClose={() => onCloseModal(setTripPublishSettingsOpen)}
-                form={FORM_TRIP_PUBLISH_SETTINGS}
-              />
-            )}
-            {tripTitleOpen && (
-              <TripCardModal
-                open={tripTitleOpen}
-                onClose={() => onCloseModal(setTripTitleOpen)}
-                form={FORM_TRIP_TITLE}
-              />
-            )}
-            {tripDestinationOpen && (
-              <TripCardModal
-                open={tripDestinationOpen}
-                onClose={() => onCloseModal(setTripDestinationOpen)}
-                form={FORM_TRIP_DESTINATION}
-              />
-            )}
-            {copyTripOpen && (
-              <TripCardModal
-                open={copyTripOpen}
-                onClose={() => onCloseModal(setCopyTripOpen)}
-                form={FORM_COPY_TRIP}
-              />
-            )}
-            {deleteTripOpen && (
-              <TripCardModal
-                open={deleteTripOpen}
-                onClose={() => onCloseModal(setDeleteTripOpen)}
-                form={FORM_DELETE_TRIP}
-              />
-            )}
-          </>
-        )}
-      </div>
       {isDetailPage ? (
-        <div className="p-3 text-gray-700">
-          <div className="flex justify-center items-center mb-2 text-lg">
-            {!viewMode && (
-              <FontAwesomeIcon
-                icon={trip.is_public ? faUnlock : faLock}
-                className="mr-2"
-              />
-            )}
-            <h3 className="font-bold">{trip.title}</h3>
+        <>
+          <div className="relative pb-[66.666667%]">
+            <div className="absolute inset-0">{tripImage}</div>
           </div>
-          <div className="flex justify-between text-sm">
-            <p className="text-gray-500">
-              {formatDate(trip.start_date)}-{formatDate(trip.end_date)}
-            </p>
-            <span>
-              <FontAwesomeIcon icon={faLocationDot} className="mr-1" />
-              {TRIP_DESTINATION_ITEMS[trip.prefecture_id - 1]?.name}
-            </span>
-          </div>
-        </div>
+          <div className="p-3 text-gray-700">{tripDescription}</div>
+        </>
       ) : (
-        <Link href={`${TRIPS_URL}/${trip.trip_token}`}>
-          <div className="p-3 text-gray-700">
-            <div className="flex justify-center items-center mb-2 text-sm">
-              <FontAwesomeIcon
-                icon={trip.is_public ? faUnlock : faLock}
-                className="mr-2"
-              />
-              <h3 className="overflow-hidden whitespace-nowrap text-ellipsis font-bold">
-                {trip.title}
-              </h3>
+        <Link
+          href={`${TRIPS_URL}/${trip.trip_token}`}
+          legacyBehavior
+          key={trip.id}
+        >
+          <a className="border-2 border-transparent focus:border-brand-color block outline-none rounded-lg">
+            <div className="relative pb-[66.666667%]">
+              <div className="absolute inset-0">{tripImage}</div>
             </div>
-            <div className="flex justify-between text-xs">
-              <p className="text-gray-500">
-                {formatDate(trip.start_date)}-{formatDate(trip.end_date)}
-              </p>
-              <span>
-                <FontAwesomeIcon icon={faLocationDot} className="mr-1" />
-                {TRIP_DESTINATION_ITEMS[trip.prefecture_id - 1]?.name}
-              </span>
-            </div>
-          </div>
+            <div className="p-3 text-gray-700">{tripDescription}</div>
+          </a>
         </Link>
       )}
+
+      {!viewMode && dropDownMenu}
     </div>
   )
 }
